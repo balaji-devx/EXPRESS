106138b1ebd5db3352363f71ab94d96bbc69c765
25mx101 <25mx101@MCA.PSG>
2025-11-05 15:35:50 +0530

    product

--- app.js
+++ app.js
@@ -4,8 +4,33 @@ const port = 3000;
 
 
 app.get("/", (req, res) => {
+        throw new Error("Test error");
     res.send("Hello World!");
 });
+
+const errorHandler = (err, req, res, next) => {
+  console.error('Error:', err.message);
+  console.error(err.stack);
+ 
+  res.status(err.status || 500);
+ 
+  // Check if request accepts JSON
+  if (req.accepts('json') && !req.accepts('html')) {
+    res.json({
+      error: {
+        message: err.message,
+        status: err.status || 500
+      }
+    });
+  } else {
+    res.render('error', {
+      title: 'Error',
+      error: err.message
+    });
+  }
+};
+app.use(errorHandler);
+
 
 
 app.listen(port, () => {
